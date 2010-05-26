34fb91a0ade7d8df29345b1116192a9170751646
bartosz.kedra <bartosz.kedra@090aa83f-14d9-ed16-b29d-d12b862dbd3d>
2010-05-26 09:07:52 +0000

    arm-gui-framework: Implemented UnifiedGraphics abstract class to unify the interface of OpenGlGraphics and SDLGraphics in sdljava. This will help to get rid of 'instanceof' statements in code concerning Graphics object.

---
+++ src/platform/gfx/UnifiedGraphics.java
@@ -0,0 +1,34 @@
+package platform.gfx;
+
+import sdljava.video.SDLRect;
+import sdljava.video.SDLSurface;
+import sdljavax.guichan.gfx.Graphics;
+
+public abstract class UnifiedGraphics extends Graphics{
+
+	//SDLGraphics methods
+	public void	drawSDLSurface(SDLSurface surface, SDLRect source, SDLRect destination){
+		throw new UnsupportedOperationException();
+	}
+	
+	public SDLSurface getTarget(){	
+		throw new UnsupportedOperationException();
+	}
+	
+	public void	setTarget(SDLSurface target){
+		throw new UnsupportedOperationException();
+	}
+	
+	protected void	drawHLine(int x1, int y, int x2){
+		throw new UnsupportedOperationException();
+	}
+	protected void	drawVLine(int x, int y1, int y2){
+		throw new UnsupportedOperationException();
+	}
+	
+	//OpenGlGraphics method
+	public void setTargetPlane(int width, int height){
+		throw new UnsupportedOperationException();
+	}
+	
+}
