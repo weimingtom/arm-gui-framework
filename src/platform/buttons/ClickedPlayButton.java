066ec8cc8cf70fb22e10a13e1b335484eee46224
bartosz.kedra <bartosz.kedra@090aa83f-14d9-ed16-b29d-d12b862dbd3d>
2010-05-28 17:27:48 +0000

    arm-gui-framework: Implemented three exemplary ButtonState classes.

---
+++ src/platform/buttons/ClickedPlayButton.java
@@ -0,0 +1,39 @@
+package platform.buttons;
+
+import platform.gui.ButtonState;
+import platform.util.UpdateListener;
+import sdljavax.guichan.GUIException;
+import sdljavax.guichan.gfx.Image;
+
+public class ClickedPlayButton extends ButtonState{
+
+		
+	
+	public ClickedPlayButton(String imageStr, UpdateListener listener)throws GUIException {
+		super(imageStr, listener);
+	
+	}
+
+	
+	public void mouseIn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseOut() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseRelease() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
