4262330490d3a30eff8a8c2c89942e1f16ef207e
bartosz.kedra <bartosz.kedra@090aa83f-14d9-ed16-b29d-d12b862dbd3d>
2010-05-28 17:27:48 +0000

    arm-gui-framework: Implemented three exemplary ButtonState classes.

---
+++ trunk/src/platform/buttons/DefaultPlayButton.java
@@ -0,0 +1,37 @@
+package platform.buttons;
+
+import platform.gui.ButtonState;
+import platform.util.UpdateListener;
+import sdljavax.guichan.GUIException;
+import sdljavax.guichan.gfx.Image;
+
+public class DefaultPlayButton extends ButtonState{
+
+		
+	public DefaultPlayButton(String imageStr, UpdateListener listener)throws GUIException {
+		super(imageStr, listener);
+		
+	}
+
+	
+	public void mouseIn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseOut() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseRelease() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ trunk/src/platform/buttons/SelectedPlayButton.java
@@ -0,0 +1,35 @@
+package platform.buttons;
+
+import platform.gui.ButtonState;
+import platform.util.UpdateListener;
+import sdljavax.guichan.GUIException;
+import sdljavax.guichan.gfx.Image;
+
+public class SelectedPlayButton extends ButtonState{
+	
+	public SelectedPlayButton(String imageStr, UpdateListener updateListener) throws GUIException{
+		super(imageStr, updateListener);
+	}
+	
+	
+	public void mouseIn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseOut() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mousePress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void mouseRelease() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
