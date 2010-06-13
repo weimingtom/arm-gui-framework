02074fa31fe2b1c772433f514fecff63e93d36fe
bartosz.kedra <bartosz.kedra@090aa83f-14d9-ed16-b29d-d12b862dbd3d>
2010-06-13 10:33:20 +0000

    FlipEffectHandler: Changed way of changing Areas. Panel: Added memory of last alpha index set.

--- src/platform/buttons/ClickedPlayButton.java
+++ src/platform/buttons/ClickedPlayButton.java
@@ -2,8 +2,11 @@ package platform.buttons;
 
 import platform.gui.Button;
 import platform.gui.ButtonState;
+import platform.gui.Screen;
+import platform.thread.FlipEffectHandler;
 import platform.util.UpdateListener;
 import platform.util.WidgetUpdate;
+import sdljava.SDLException;
 import sdljava.video.SDLRect;
 import sdljavax.guichan.GUIException;
 
@@ -29,7 +32,11 @@ public class ClickedPlayButton extends ButtonState{
 		parentButton.setCurrentState(parentButton.getDefaultButton());
 		try {
 			updateListener.putRegionToUpdate(new WidgetUpdate(parentButton,new SDLRect(parentButton.getX(),parentButton.getY(),buttonImage.getWidth(), buttonImage.getHeight())));
+			new FlipEffectHandler(Screen.getScreen().getForeground());
 		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SDLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}	
--- src/platform/thread/FlipEffectHandler.java
+++ src/platform/thread/FlipEffectHandler.java
@@ -1,6 +1,7 @@
 package platform.thread;
 
 import platform.gui.Area;
+import platform.gui.Screen;
 import platform.util.UpdateListener;
 import platform.util.WidgetUpdate;
 import sdljava.SDLException;
@@ -11,13 +12,16 @@ import sdljavax.gfx.SDLGfx;
 public class FlipEffectHandler extends Thread{
 
 	final int TRANSITION_NR=10;
-	Area motionArea;
+	Area motionArea, anotherArea;
 	UpdateListener updateListener;
 	SDLSurface destSurface, originalSurface;
 	int xCenter, yCenter, maxHorizontalRadius, maxVerticalRadius;
 	
-	public FlipEffectHandler(Area area){
+	public FlipEffectHandler(Area area) throws SDLException{
+		
 		motionArea = area;
+		
+		anotherArea = (Screen.getScreen().getBackground().equals(motionArea)) ? Screen.getScreen().getForeground() : Screen.getScreen().getBackground();
 		updateListener = (UpdateListener) area;
 		destSurface = area.getSurface();
 		
@@ -26,35 +30,35 @@ public class FlipEffectHandler extends Thread{
 		yCenter = destSurface.getHeight() / 2;
 		maxHorizontalRadius = destSurface.getWidth() / 4;
 		maxVerticalRadius = destSurface.getHeight() / 4;
+		
+		displayArea(motionArea);
+		
 		start();
 	}
 	
 	public void run(){
 		int r=255,g=0,b=0;
 		int i=0;
-		
-		
+				
 		try{
 		
 			
-			while( i++ < 20){
+			while( i++ < 4){
 				
 				
-				SDLGfx.filledEllipseRGBA(destSurface, xCenter, yCenter, maxHorizontalRadius , maxVerticalRadius, 10, 10, 10 ,255);
-				updateListener.putRegionToUpdate(new WidgetUpdate(motionArea, new SDLRect(xCenter - maxHorizontalRadius, yCenter - maxVerticalRadius, maxHorizontalRadius *2, maxVerticalRadius * 2 )));
-				
+				//SDLGfx.filledEllipseRGBA(destSurface, xCenter, yCenter, maxHorizontalRadius , maxVerticalRadius, 10, 10, 10 ,255);
+				//updateListener.putRegionToUpdate(new WidgetUpdate(motionArea, new SDLRect(xCenter - maxHorizontalRadius, yCenter - maxVerticalRadius, maxHorizontalRadius *2, maxVerticalRadius * 2 )));
 				for(int j=TRANSITION_NR; j > (-1) ; j--){
-					
 					
 					//parametr alpha 0 to pełna przeżroczystość
 					SDLGfx.filledEllipseRGBA(destSurface, xCenter, yCenter, maxHorizontalRadius / TRANSITION_NR * j, maxVerticalRadius, r, g, b ,255);
+					
 					updateListener.putRegionToUpdate(new WidgetUpdate(motionArea, new SDLRect(xCenter - maxHorizontalRadius, yCenter - maxVerticalRadius, maxHorizontalRadius *2, maxVerticalRadius * 2 )));
 					
 					Thread.sleep( (int)((double)(j + 1) / TRANSITION_NR * 150));
 					//updateListener.putRegionToUpdate(new WidgetUpdate(motionArea, new SDLRect(xCenter - maxHorizontalRadius, yCenter - maxVerticalRadius, maxHorizontalRadius *2, maxVerticalRadius * 2 )));
 
-					
-					SDLGfx.filledEllipseRGBA(destSurface, xCenter, yCenter, maxHorizontalRadius / TRANSITION_NR * j, maxVerticalRadius, 10, 10, 10 ,255);
+					SDLGfx.filledEllipseRGBA(destSurface, xCenter, yCenter, maxHorizontalRadius / TRANSITION_NR * j, maxVerticalRadius, 255, 255, 255 ,255);
 					updateListener.putRegionToUpdate(new WidgetUpdate(motionArea, new SDLRect(xCenter - maxHorizontalRadius, yCenter - maxVerticalRadius, maxHorizontalRadius *2, maxVerticalRadius * 2 )));	
 					
 				}
@@ -65,7 +69,6 @@ public class FlipEffectHandler extends Thread{
 					updateListener.putRegionToUpdate(new WidgetUpdate(motionArea, new SDLRect(xCenter - maxHorizontalRadius, yCenter - maxVerticalRadius, maxHorizontalRadius *2, maxVerticalRadius * 2 )));
 					Thread.sleep( (int)((double) (k + 1) / TRANSITION_NR * 150));
 				}
-				
 				Thread.sleep(1500);
 			}
 			
@@ -75,6 +78,25 @@ public class FlipEffectHandler extends Thread{
 		catch(InterruptedException e){
 			e.printStackTrace();
 		} 
+		displayArea(anotherArea);
+				
+	}
+	
+	private void displayArea(Area display){
+		int x=241;
+		
+		while( x > 0){
+			
+			try {
+				display.setAlpha(255-x);
+				Thread.sleep(100);
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			x-=12;
+		}
 	}
 	
 }
