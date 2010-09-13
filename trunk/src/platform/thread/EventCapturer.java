3aaf590f5b5d551aa759e4f320fe56e723725e80
bartosz.kedra <bartosz.kedra@090aa83f-14d9-ed16-b29d-d12b862dbd3d>
2010-09-13 16:04:53 +0000

    arm-gui-framework: Javadoc style comments part three.

--- trunk/src/platform/thread/EventCapturer.java
+++ trunk/src/platform/thread/EventCapturer.java
@@ -1,19 +1,70 @@
 package platform.thread;
 
+/**
+*  arm-gui-framework -Java GUI based on sdljava for omap5912 board
+*  Copyright (C) 2010  Bartosz Kędra (bartosz.kedra@gmail.com)
+* 
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 3.0 of the License, or (at your option) any later version.
+* 
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+* 
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+*  USA
+*
+*/
+
 import platform.evt.ExtendedInput;
 import platform.gui.Screen;
 import sdljava.SDLException;
 import sdljava.event.SDLEvent;
 import sdljava.event.SDLKey;
 import sdljava.event.SDLKeyboardEvent;
+/**
+ * Class serving as constantly running thread that captures the events
+ * @author Bartosz Kędra
+ * @author bartosz.kedra@gmail.com
+ *
+ */
 
 public class EventCapturer extends Thread{
 
+	/**
+	 * Input event source
+	 */
 	private ExtendedInput inputSource;
+	
+	/**
+	 * Event object
+	 */
 	private SDLEvent event;
+	
+	/**
+	 * Thread that dispatches events to appriopriate objects
+	 * <code> EventDispatcher </code>
+	 */
 	private EventDispatcher eventDispatcher;
+	
+	/**
+	 * Flag indicating whether input event was captured
+	 */
 	private boolean eventCaptured;
 	
+	/**
+	 * Constructor also starts the thread
+	 * @param input
+	 * 			Input event source
+	 * @param dispatcher
+	 * 			Event dispatcher
+	 * @throws SDLException
+	 */
 	public EventCapturer(ExtendedInput input, EventDispatcher dispatcher) throws SDLException {
 		super ("EventCapturer");
 		inputSource = input;
@@ -26,6 +77,11 @@ public class EventCapturer extends Thread{
 		start();
 	}
 	
+	/**
+	 * Thread polls for events, once event occurs it is analyzed ( if ESC the app finishes)
+	 * and then it is put into event queue of inputSource and eventDispatcher is notified
+	 * 
+	 */
 	public void run(){
 		try {
 			while (! Thread.interrupted()) {
--- trunk/src/platform/thread/FlipEffectHandler.java
+++ trunk/src/platform/thread/FlipEffectHandler.java
@@ -1,5 +1,25 @@
 package platform.thread;
 
+/**
+*  arm-gui-framework -Java GUI based on sdljava for omap5912 board
+*  Copyright (C) 2010  Bartosz Kędra (bartosz.kedra@gmail.com)
+* 
+*  This library is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU Lesser General Public
+*  License as published by the Free Software Foundation; either
+*  version 3.0 of the License, or (at your option) any later version.
+* 
+*  This library is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+*  Lesser General Public License for more details.
+* 
+*  You should have received a copy of the GNU Lesser General Public
+*  License along with this library; if not, write to the Free Software
+*  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+*  USA
+*
+*/
 import platform.gui.Area;
 import platform.gui.Screen;
 import platform.util.UpdateListener;
@@ -9,19 +29,72 @@ import sdljava.video.SDLRect;
 import sdljava.video.SDLSurface;
 import sdljavax.gfx.SDLGfx;
 
+/**
+ * Thread prepared for showing capabilities of library, it rotates an ellipse on the screen
+ * what imitates 3D rotation effect
+ * @author Bartosz Kędra
+ * @author bartosz.kedra@gmail.com
+ *
+ */
 public class FlipEffectHandler extends Thread {
 
+	/**
+	 * Number of rotations 
+	 */
 	final int TRANSITION_NR = 10;
+	
+	/**
+	 * Area where the rotations take place
+	 */
 	Area motionArea;
+	
+	/**
+	 * Second area, which fist becomes invisible and then visible again after effect finishes
+	 */
 	Area anotherArea;
+	
+	/**
+	 * Object processing update requests
+	 */
 	UpdateListener updateListener;
+	
+	/**
+	 * Motion area surface
+	 */
 	SDLSurface destSurface;
+	
+	/**
+	 * Original motion surface, preserved
+	 */
 	SDLSurface originalSurface;
+	
+	/**
+	 * Center x coordinate of an ellipse
+	 */
 	int xCenter;
+	
+	/**
+	 * Center y coordinate of an ellipse
+	 */
 	int yCenter;
+	
+	/**
+	 * horizontal ellipse radius
+	 */
 	int maxHorizontalRadius;
+	
+	/**
+	 * vertical ellipse radius
+	 */
 	int maxVerticalRadius;
 	
+	
+	/**
+	 * Constructor, also launches the thread
+	 * @param area
+	 * 			motion area
+	 * @throws SDLException
+	 */
 	public FlipEffectHandler(Area area) throws SDLException {
 		motionArea = area;
 		
@@ -39,6 +112,11 @@ public class FlipEffectHandler extends Thread {
 		start();
 	}
 	
+	/**
+	 * Function setting transparency coefficient to surface
+	 * @param display
+	 * 			Area that should become visible
+	 */
 	private void displayArea(Area display) {
 		int x = 250;
 		
@@ -55,6 +133,10 @@ public class FlipEffectHandler extends Thread {
 		display.setAlpha(255);
 	}
 	
+	/**
+	 * Main thread function rotating ellipse
+	 * 
+	 */
 	public void run(){
 		int r = 255;
 		int g = 0;
@@ -66,7 +148,7 @@ public class FlipEffectHandler extends Thread {
 				//SDLGfx.filledEllipseRGBA(destSurface, xCenter, yCenter, maxHorizontalRadius , maxVerticalRadius, 10, 10, 10 ,255);
 				//updateListener.putRegionToUpdate(new WidgetUpdate(motionArea, new SDLRect(xCenter - maxHorizontalRadius, yCenter - maxVerticalRadius, maxHorizontalRadius *2, maxVerticalRadius * 2 )));
 				for (int j = TRANSITION_NR; j > (-1); j--) {
-					//parametr alpha 0 to pełna przeżroczystość
+					//parametr alpha 0 means fully transparent 
 					SDLGfx.filledEllipseRGBA(destSurface, xCenter, yCenter, maxHorizontalRadius / TRANSITION_NR * j, maxVerticalRadius, r, g, b ,255);
 					
 					updateListener.putRegionToUpdate(new WidgetUpdate(motionArea, new SDLRect(xCenter - maxHorizontalRadius, yCenter - maxVerticalRadius,
